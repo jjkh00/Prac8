4e6b42290ad5b07ef9fee51114925ff55f6b7ffc
Khoo Jing Jie <jjkhoo.18@ichat.sp.edu.sg>
2019-10-29 14:59:48 +0800

    Prac8 Branch Ex2 - added calculator

---
+++ src/java/services/AddNum.java
@@ -0,0 +1,78 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package services;
+
+import javax.ws.rs.core.Context;
+import javax.ws.rs.core.UriInfo;
+import javax.ws.rs.Consumes;
+import javax.ws.rs.Produces;
+import javax.ws.rs.GET;
+import javax.ws.rs.Path;
+import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
+import javax.ws.rs.core.MediaType;
+
+/**
+ * REST Web Service
+ *
+ * @author JJ
+ */
+@Path("calculatews")
+public class AddNum {
+
+    @Context
+    private UriInfo context;
+
+    /**
+     * Creates a new instance of AddNum
+     */
+    public AddNum() {
+    }
+
+    /**
+     * Retrieves representation of an instance of services.AddNum
+     * @param num1
+     * @param num2
+     * @return an instance of java.lang.String
+     */
+    @GET
+    @Path("add")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String addNum(@QueryParam("num1") int num1,@QueryParam("num2") int num2) {
+        //TODO return proper representation object
+        return "the sum of "+num1+" and "+num2+" is: "+(num1+num2);
+    }
+    @GET
+    @Path("sub")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String subNum(@QueryParam("num1") int num1,@QueryParam("num2") int num2) {
+        //TODO return proper representation object
+        return "the subtraction of "+num1+" and "+num2+" is: "+(num1-num2);
+    }
+    @GET
+    @Path("muliply")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String mulNum(@QueryParam("num1") int num1,@QueryParam("num2") int num2) {
+        //TODO return proper representation object
+        return "the multiplication of "+num1+" and "+num2+" is: "+(num1*num2);
+    }
+    @GET
+    @Path("divide")
+    @Produces(MediaType.APPLICATION_JSON)
+    public String divNum(@QueryParam("num1") int num1,@QueryParam("num2") int num2) {
+        //TODO return proper representation object
+        return "the division of "+num1+" and "+num2+" is: "+(num1/num2);
+    }
+
+    /**
+     * PUT method for updating or creating an instance of AddNum
+     * @param content representation for the resource
+     */
+    @PUT
+    @Consumes(MediaType.APPLICATION_XML)
+    public void putXml(String content) {
+    }
+}
