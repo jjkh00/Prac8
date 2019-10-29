dd62eb40190143b79262699127a34e1443e09430
Khoo Jing Jie <jjkhoo.18@ichat.sp.edu.sg>
2019-10-29 14:03:04 +0800

    Prac8 Branch Ex1.1 - added queryParam username

--- src/java/services/HelloWS.java
+++ src/java/services/HelloWS.java
@@ -12,6 +12,7 @@ import javax.ws.rs.Produces;
 import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.PUT;
+import javax.ws.rs.QueryParam;
 import javax.ws.rs.core.MediaType;
 
 /**
@@ -33,6 +34,7 @@ public class HelloWS {
 
     /**
      * Retrieves representation of an instance of services.HelloWS
+     * @param username
      * @return an instance of java.lang.String
      */
     /*@GET
@@ -45,9 +47,9 @@ public class HelloWS {
     @GET
     @Path("sayHello")
     @Produces("application/json")
-    public String sayHello() {
+    public String sayHello(@QueryParam("username") String username) {
         //TODO return proper representation object
-        return "Hello there!";
+        return "Hello there!... "+username;
     }
 
 
